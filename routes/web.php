500117b46a022b5ceb1417b126c704d163b7ae67
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-09-25 12:42:39 +0530

    fix: correct contact route definition in web.php

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,9 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('frontend.about');
 });
-
+Route::get('/contact', function () {
+    return view('frontend.contact');
+})->name('contact');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
