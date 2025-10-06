7b3787d53556cc72bb4d17acfd071b556648fb44
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-10-06 10:26:22 +0530

    fix: update menu link text from 'Menu' to 'Products'

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -12,7 +12,7 @@
             <ul>
                 <li><a href="/" class="active">Home</a></li>
                 <li><a href="/about">About</a></li>
-                <li><a href="/menu">Menu</a></li>
+                <li><a href="/menu">Products</a></li>
 
                 </li>
                 <li><a href="/contact">Contact</a></li>
